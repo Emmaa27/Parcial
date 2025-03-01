bcfd7c2571172efed49ec64453a9c95287aeeb61
Emma Romero <emma.ra09@gmail.com>
2025-03-01 11:58:10 -0500

    v.05

--- program.js
+++ program.js
@@ -20,20 +20,80 @@ async function loadPolygon() {
 }
 loadPolygon();
 
-async function loadTrees() {
-    let myData = await fetch ('arboles_elplan.geojson');
-    let myPolygon = await myData.json();
-
-    L.geoJSON(myPolygon,
-        {
-            style: {
-                color: 'orange'
-            }
-        }
-    ).addTo(map);
-    
-}
-loadTrees();
 let btnTrees = document.getElementById('btnTrees');
 
-btnTrees.addEventListener('click', ()=> alert("Hola"));
+btnTrees.addEventListener('click',
+    async ()=>{
+        let response = await fetch("arboles_elplan.geojson");
+        let datos = await response.json();
+        
+        L.geoJSON(
+            datos, 
+            {
+                pointToLayer: (feature, latlong)=>{
+                    return L.circleMarker(latlong,{
+                        radius:5,
+                        fillColor:'green',
+                        weight:1,
+                        fillOpacity:0.5,
+                    })
+
+                }
+            }
+        ).addTo(map)
+    }
+    )
+
+
+let btnDistance = document.getElementById('btnDistance');
+
+btnDistance.addEventListener('click',
+    async ()=>{
+        let response = await fetch("arboles_elplan.geojson");
+        let datos = await response.json();
+        let trees = datos.features.map((myElement, index)=>({
+            id: index+1,
+            coordinates: myElement.geometry.coordinates
+        }));
+        
+        console.log(trees);
+
+        let distances=[];
+        trees.forEach((treeA)=>{trees.forEach
+            (
+                (treeB)=>{
+                    if (treeA.id != treeB.id){
+                        let distance =turf.distance(
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push(
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)
+                            ]
+                        )   
+                    }
+                }
+            )
+        })
+        generatePDF(distances,trees.lenght);
+    }
+)
+
+function generatePDF(distances, totalTrees){
+    let { jsPDF } = window.jspdf;
+    let documentPDF = new jsPDF();
+console.log('pase por aca', distances)
+    documentPDF.text("Reporte de árboles El Plan", 10,10)
+    
+    documentPDF.autoTable(
+        {
+        head: [['Árbol 1', 'Árbol 2', 'Distance']],
+        body: distances
+        }
+    );
+    console.log('pase por aca2', documentPDF)
+    documentPDF.save("El_Plan.pdf")
+}
